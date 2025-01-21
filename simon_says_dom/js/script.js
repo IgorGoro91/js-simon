31ee69ab5ed168dff47e187357b61cfa281ed69a
IgorGoro91 <igorysc@gmail.com>
2025-01-21 23:06:03 +0100

    add:non mi resulta layout

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,11 @@
+
+let conter = 30
+
+const btn = document.getElementsByClassName(`btn`)
+const countdown =document.getElementById(`countdown`)
+
+btn.addEventListener(`click`, function ){
+
+    console.log("ciao")
+}
+
